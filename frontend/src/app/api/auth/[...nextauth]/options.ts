8353e1c9813a44cceb29c60e49ac6c97c8f7147f
geekyharsh05 <hp424805@gmail.com>
2024-11-03 02:25:42 +0530

    feat: Enhance authentication options

--- frontend/src/app/api/auth/[...nextauth]/options.ts
+++ frontend/src/app/api/auth/[...nextauth]/options.ts
@@ -1,18 +1,19 @@
 import { AuthOptions, ISODateString } from "next-auth";
+import { JWT } from "next-auth/jwt";
 import GoogleProvider from "next-auth/providers/google";
 
 export interface CustomSession {
     user?: CustomUser;
     expires: ISODateString;
   }
-  export interface CustomUser {
+export interface CustomUser {
     id?: string | null;
     name?: string | null;
     email?: string | null;
     image?: string | null;
     provider?: string | null;
     token?: string | null;
-  }
+}
 
 export const authOptions: AuthOptions = {
     pages: {
@@ -32,4 +33,25 @@ export const authOptions: AuthOptions = {
           },
         }),
     ],
+
+    callbacks: {
+
+      async signIn({ user, account }) {
+        console.log("The user data is", user);
+        console.log('The account is', account);
+        return true;
+      },
+
+      async session({ session, user, token }: { session: CustomSession, user: CustomUser, token: JWT}) {
+        session.user = token.user as CustomUser;
+        return session;
+      },
+
+      async jwt({ token, user }) {
+        if (user) {
+          token.user = user;
+        }
+        return token;
+      },
+    }
 }
