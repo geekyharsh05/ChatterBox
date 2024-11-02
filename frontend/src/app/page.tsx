d0133e2d4d573e9b20dfefed92a8abf70e8d17ee
geekyharsh05 <hp424805@gmail.com>
2024-11-03 02:25:28 +0530

    feat: Pass user session to Navbar

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,11 +2,15 @@ import FeatureSection from "@/components/core/feature-section";
 import Footer from "@/components/core/footer";
 import HeroSection from "@/components/core/hero-section";
 import Navbar from "@/components/core/navbar";
+import { authOptions, CustomSession } from "./api/auth/[...nextauth]/options";
+import { getServerSession } from "next-auth";
 
-export default function Home() {
+export default async function Home() {
+  const session: CustomSession | null = await getServerSession(authOptions);
+
   return (
     <div className="min-h-screen flex flex-col">
-      <Navbar />
+      <Navbar user={session?.user} />
       <HeroSection />
       <FeatureSection />
       <Footer />
