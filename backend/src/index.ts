d0e9fdeacea0bd638ab9e7f41d6b94582c2ba45d
geekyharsh05 <hp424805@gmail.com>
2024-11-27 00:47:31 +0530

    feat: Update package.json with Prisma scripts and add custom types for AuthUser interface

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express, { Application, Request, Response } from "express";
 import "dotenv/config";
 import cors from "cors";
 import morgan from "morgan";
+import router from "./routes/index.js";
 const app: Application = express();
 const PORT = process.env.PORT || 8000;
 
@@ -11,8 +12,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
 
-app.get("/", (req: Request, res: Response) => {
-  return res.send("It's working 🙌");
-});
+app.use("/api", router)
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
