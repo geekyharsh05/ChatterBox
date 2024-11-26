6c93ec0cafb3b442155cd3d9d32496e8662ed2ba
geekyharsh05 <hp424805@gmail.com>
2024-11-27 03:38:17 +0530

    feat: Add GroupChatCard component to display chat groups in dashboard

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -4,6 +4,7 @@ import { getServerSession } from 'next-auth';
 import DashNav from '@/components/chat/dash-nav';
 import CreateChat from '@/components/chatgroup/create-chat';
 import { fetchChatGroups } from '@/fetch/group-fetch';
+import GroupChatCard from '@/components/chatgroup/group-chat-card';
 
 export default async function dashboard() {
   const session: CustomSession | null = await getServerSession(authOptions);
@@ -19,6 +20,13 @@ export default async function dashboard() {
         <div className='flex justify-end mt-10'>
           <CreateChat user={session?.user!}/>
         </div>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {groups.length > 0 &&
+            groups.map((item, index) => (
+              <GroupChatCard group={item} key={index} user={session?.user!} />
+            ))}
       </div>
 
     </div>
