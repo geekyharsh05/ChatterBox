2d116ff2fc9241d081e022970f48d8c792691966
geekyharsh05 <hp424805@gmail.com>
2024-11-26 20:18:46 +0530

    feat: Add rich colors and close button to Toaster component in RootLayout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -32,8 +32,8 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <Toaster richColors duration={10000} closeButton/>
             {children}
-            <Toaster />
           </ThemeProvider>
         </body>
       </SessionProvider>
