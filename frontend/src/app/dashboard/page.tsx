fab1fbd158bb877ec87d772b17baf4abe6b19c52
geekyharsh05 <hp424805@gmail.com>
2024-11-27 02:38:31 +0530

    feat: Integrate CreateChat component and fetch chat groups in dashboard

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -2,16 +2,25 @@ import React from 'react'
 import { authOptions, CustomSession } from '../api/auth/[...nextauth]/options';
 import { getServerSession } from 'next-auth';
 import DashNav from '@/components/chat/dash-nav';
+import CreateChat from '@/components/chatgroup/create-chat';
+import { fetchChatGroups } from '@/fetch/group-fetch';
 
 export default async function dashboard() {
   const session: CustomSession | null = await getServerSession(authOptions);
+  const groups: Array<GroupChatType> | [] = await fetchChatGroups(session?.user?.token!)
+  
   return (
     <div>
-      {JSON.stringify(session?.user?.token)}
       <DashNav
         name={session?.user?.name!}
         image={session?.user?.image ?? undefined}
       />
+      <div className='container'>
+        <div className='flex justify-end mt-10'>
+          <CreateChat user={session?.user!}/>
+        </div>
+      </div>
+
     </div>
   );
 }
