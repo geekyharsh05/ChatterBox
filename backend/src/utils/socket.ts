4306cfc2e6f446944129539186ccd713744b7e37
geekyharsh05 <hp424805@gmail.com>
2024-11-28 01:53:50 +0530

    feat: Enhance socket connection handling with room validation and broadcasting

--- backend/src/utils/socket.ts
+++ backend/src/utils/socket.ts
@@ -1,12 +1,28 @@
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
+
+interface CustomSocket extends Socket {
+    room?: string
+}
 
 export function setupSocket (io: Server) {
-    io.on("connection", (socket) => {
+    io.use((socket: CustomSocket, next) => {
+        const room = socket.handshake.auth.room
+        
+        if (!room) {
+            return next(new Error("Invalid room"))
+        }
+
+        socket.room = room;
+        next();
+    })
+
+    io.on("connection", (socket: CustomSocket) => {
+        socket.join(socket.room)
         console.log("The socket is connected", socket.id)
 
         socket.on("message", (data) => {
             console.log("server side message", data)
-            socket.broadcast.emit("message", data)
+            io.to(socket.room).emit("message", data)
         })
 
         socket.on("disconnect", () => {
