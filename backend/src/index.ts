efc22593a215e8ae16cc06722629bb9e5665fc74
geekyharsh05 <hp424805@gmail.com>
2024-11-28 00:40:14 +0530

    feat: Set up Socket.IO with Redis adapter for real-time communication

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,8 +3,24 @@ import "dotenv/config";
 import cors from "cors";
 import morgan from "morgan";
 import router from "./routes/index.js";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import { setupSocket } from "./utils/socket.js";
+import { createAdapter } from "@socket.io/redis-streams-adapter";
+import redis from "./config/redis.config.js";
+
 const app: Application = express();
 const PORT = process.env.PORT ?? 8000;
+const server = createServer(app);
+
+export const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+  adapter: createAdapter(redis)
+});
+
+setupSocket(io)
 
 // * Middleware
 app.use(cors());
@@ -14,4 +30,4 @@ app.use(morgan("dev"));
 
 app.use("/api", router)
 
-app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
+server.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
