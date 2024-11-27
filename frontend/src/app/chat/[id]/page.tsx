18ef4b978459bfc4a6bec746bba4e5eeea560538
geekyharsh05 <hp424805@gmail.com>
2024-11-28 00:40:53 +0530

    feat: Update chat page to display chat base component and group ID

--- frontend/src/app/chat/[id]/page.tsx
+++ frontend/src/app/chat/[id]/page.tsx
@@ -1,11 +1,15 @@
+import ChatBase from '@/components/chat/chat-base'
 import React from 'react'
 
-type Props = {}
+const chat = ({ params }: { params: { id: string } }) => {
+  console.log("The groupd id is", params.id)
 
-const page = (props: Props) => {
   return (
-    <div>page</div>
+    <div>
+      <h1>Hello from chat</h1>
+      <ChatBase />
+    </div>
   )
 }
 
-export default page
+export default chat
