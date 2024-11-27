0b512d2bacac93a8aab695775f36c08e9e217f30
geekyharsh05 <hp424805@gmail.com>
2024-11-28 01:53:36 +0530

    feat: Integrate @socket.io/admin-ui for enhanced Socket.IO management

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,6 +8,7 @@ import { createServer } from "http";
 import { setupSocket } from "./utils/socket.js";
 import { createAdapter } from "@socket.io/redis-streams-adapter";
 import redis from "./config/redis.config.js";
+import { instrument } from "@socket.io/admin-ui";
 
 const app: Application = express();
 const PORT = process.env.PORT ?? 8000;
@@ -15,12 +16,17 @@ const server = createServer(app);
 
 export const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: ["http://localhost:3000", "https://admin.socket.io"]
   },
   adapter: createAdapter(redis)
 });
 
 setupSocket(io)
+
+instrument(io, {
+  auth: false,
+  mode: "development",
+});
 
 // * Middleware
 app.use(cors());
