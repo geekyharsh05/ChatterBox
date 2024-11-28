4b173623dd265c7f61fcc00f53d1325b482370d4
geekyharsh05 <hp424805@gmail.com>
2024-11-29 01:44:33 +0530

    feat: Add functions to fetch chat group details and users by group ID

--- frontend/src/fetch/group-fetch.ts
+++ frontend/src/fetch/group-fetch.ts
@@ -22,3 +22,39 @@ export async function fetchChatGroups(token: string) {
 
     return [];
 }
+
+export async function fetchChatGroup(id: string) {
+    const res = await fetch(`${API_ENDPOINTS.CHAT_GROUP}/${id}`, {
+        cache: "no-cache"
+    })
+
+    if (!res.ok) {
+        throw new Error("Failed to fetch data")
+    }
+
+    const response = await res.json()
+    if (response?.data) {
+        return response?.data
+    }
+
+    return null;
+}
+
+export async function fetchChatGroupUsers(id: string) {
+    const res = await fetch(`${API_ENDPOINTS.CHAT_GROUP_USERS}?group_id=${id}`, {
+      cache: "no-cache",
+    });
+  
+    if (!res.ok) {
+      const errorText = await res.text();
+      console.error("Fetch error:", errorText);
+      throw new Error("Failed to fetch data");
+    }
+
+    const response = await res.json();
+    if (response?.data) {
+      return response?.data;
+    }
+
+    return [];
+  }
