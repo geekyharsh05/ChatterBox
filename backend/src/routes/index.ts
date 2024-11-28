2f59818386c44204756b681e32a4086db7505c01
geekyharsh05 <hp424805@gmail.com>
2024-11-29 01:44:09 +0530

    feat: Add routes for ChatGroupUser and Chats controllers

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import AuthController from "../controllers/auth.controller.js";
 import ChatGroupController from "../controllers/chatgroup.controller.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
+import ChatGroupUserController from "../controllers/chatgroupuser.controller.js";
+import ChatsController from "../controllers/chats.controller.js";
 
 const router = Router();
 
@@ -14,5 +16,12 @@ router.get("/chat-group/:id", ChatGroupController.show);
 router.post("/chat-group", authMiddleware, ChatGroupController.store);
 router.put("/chat-group/:id", authMiddleware, ChatGroupController.update);
 router.delete("/chat-group/:id", authMiddleware, ChatGroupController.destroy);
+
+// Chat Group Users
+router.get("/chat-group-user", ChatGroupUserController.index)
+router.post("/chat-group-user", ChatGroupUserController.store)
+
+// Chats
+router.get("/chats/:groupId", ChatsController.index);
 
 export default router;
